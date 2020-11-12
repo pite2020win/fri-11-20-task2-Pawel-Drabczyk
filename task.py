3e05167a11c4f8e3c447e9cdc4cba6c17c1642f1
Pawel-Drabczyk <paweel.drabczyk@gmail.com>
2020-11-12 22:54:37 +0100

    work in progress

--- task.py
+++ task.py
@@ -29,7 +29,7 @@
 #Delete these comments before commit!
 #Good luck.
 
-
+'''
 class student:
   def __init__(self, n, sn):
     self.name = n
@@ -40,18 +40,18 @@ class student:
   def addGrade(self,g):
     self.grades.append(g)
 
-  def addPresence(self,p)
+  def addPresence(self,p):
     self.presences.append(p)
 
   def getStudentGradeAverage(self):
-    pass float(sum(self.grades))/len(self.grades) 
+    return float(sum(self.grades))/len(self.grades)
   
   def getStudentAttendance(self):
     numerator = 0
     for i in range len(self.presences): 
       if self.presences[i]:
         numerator = numerator + 1 
-    pass float(numerator)/len(self.presences)
+    return float(numerator)/len(self.presences)
 
 class Class:
   def __init__(self, n)
@@ -65,13 +65,13 @@ class Class:
     numerator = 0
     for i in range(len(self.listOfStudents))
       numerator = numerator + self.listOfStudents[i].getStudentGradeAverage()
-    pass float(numerator)/len(self.listOfStudents)
+    return float(numerator)/len(self.listOfStudents)
 
   def getClassAverageGrade(self):
     numerator = 0
     for i in range(len(self.listOfStudents))
       numerator = numerator + self.listOfStudents[i].getStudentAttendance()
-    pass float(numerator)/len(self.listOfStudents)
+    return float(numerator)/len(self.listOfStudents)
 
 class School:
   def __init__(self, n):
@@ -80,3 +80,99 @@ class School:
 
   def addClass(self, c)
     self.listOfClasses.append(c)
+'''
+
+import random
+import logging
+import json
+import statistics as stat
+
+def calculateAverage(dict):
+  averages = []
+  if "schoolsList" in dict:
+    averagesList = calculateAverage(dict["schoolsList"])
+    for i in averagesList:
+      averages.append(i)
+  elif "clasList" in dict:
+    averagesList = calculateAverage(dict["clasList"])
+    for i in averagesList:
+      averages.append(i)
+  elif "studentList" in dict:
+    averagesList = calculateAverage(dict["studentList"])
+    for i in averagesList:
+      averages.append(i)
+  elif "grades" in dict:
+    averages = dict["grades"]
+  else:
+    logging.warning(f'Watch out! Wrong type of dictonary! ')
+    return [-1]
+  return stat.mean(averages)
+
+if __name__ == '__main__':
+  logger = logging.getLogger()
+  logger.setLevel(logging.DEBUG)
+  nStudents = 15
+
+  randomNames = ["Florentyna", "Agnieszka", "Ewelina", "Edyta", "Klaudia", "Aniela", "Paula", "Angelika", "Julianna", "Adrianna", "Gabriela", "Matylda", "Jolanta", "Beata","Weronika"]
+  randomSurnames = ["Makowska", "Lewandowska", "Jakubowska", "Walczak", "Gajewska", "Zawadzaka", "Baran", "Przybylska", "Wojciechowska", "Chmielewska", "Pawlak", "Kolodziej", "Jaworska", "Zakrzewska", "Sobczak" ]
+
+  studentListTemp = []
+  for i in range(nStudents):
+    student = {
+      "name" : randomNames[i],
+      "surname" : randomSurnames[i],
+      "grades" : random.choices([1,2,3,4,5],k=5),
+      "presences" : random.choices([True,False], k=5)
+    }
+    studentListTemp.append(student)
+
+  clas1 = {
+    "name" : "1a",
+    "studentList" : [studentListTemp[0], studentListTemp[1], studentListTemp[2], studentListTemp[3], studentListTemp[4], studentListTemp[5], studentListTemp[6]]
+  }
+  clas2 = {
+    "name" : "2a",
+    "studentList" : [studentListTemp[7], studentListTemp[8], studentListTemp[9] ]
+  }
+  clas3 = {
+    "name" : "1a",
+    "studentList" : [studentListTemp[10], studentListTemp[11], studentListTemp[12], studentListTemp[13], studentListTemp[14] ]
+  }
+
+  school1 = {
+    "name" : "Liceum numer 1",
+    "clasList" : [clas1, clas2]
+  }
+  school2 = {
+    "name" : "Liceum numer 2",
+    "clasList" : [clas3]
+  }
+
+  schools = {
+    "name" : "Zespol szkol licealnych w krakowie",
+    "schoolList" : [school1, school2]
+  }
+
+  if logger.level == logging.DEBUG:
+    gradesTemp = studentListTemp[1]["grades"]
+    presencesTemp = studentListTemp[1]["presences"]
+    logging.debug(f"studentList[1].grades = {gradesTemp} studentListTemp[1].presences = {presencesTemp}")
+
+  testAverageStudent = calculateAverage( schools["schoolList"][0]["clasList"][0]["studentList"][0] )
+  logging.info(f'Testing calculateAverage function for student1  from clas1 from school1. Grades: {schools["schoolList"][0]["clasList"][0]["studentList"][0]["grades"]}, average: {testAverageStudent}')
+
+  testAveragesClas = []
+  for stud in schools["schoolList"][0]["clasList"][0]["studentList"]:
+    testAveragesClas.append( calculateAverage(stud) )
+  #testAverageClas = calculateAverage(schools["schoolList"][0]["clasList"][0])
+  testAverageClas = calculateAverage(clas1)
+
+  logging.info(f'Testing calculateAverage function for clas1 from school1. Averages of individual students: {testAveragesClas}, average of the clas {schools["schoolList"][0]["clasList"][0]["name"]}: {testAverageClas}')
+
+
+  with open('schools.txt', 'w') as outFile:
+    json.dump(schools, outFile)
+
+  with open('schools.txt') as inputFile:
+    data = json.load(inputFile)
+
